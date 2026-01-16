2569e025c1c0448720037107caac80610a3aca48
Samir-ibourki <samiribourki7@gmail.com>
2026-01-16 11:08:22 +0100

    delete js file

--- backend/src/services/index.js
+++
@@ -1,5 +0,0 @@
-// Services Index - Export all business logic services
-export * as DispatchService from './dispatchService.js';
-export * as RouteService from './routeService.js';
-export * as ReceiptService from './receiptService.js';
-export * as ZoneCacheService from './zoneCacheService.js';
